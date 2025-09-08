c20b24c584c06e4bc5dbfc862a5fd32c1dbdfd94
dinahland <hsunnies@gmail.com>
2025-09-08 19:19:42 +0900

    feat: 전역 예외 처리기 구현

---
+++ src/main/java/com/dayaeyak/performance/handler/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.dayaeyak.performance.handler;
+
+import com.dayaeyak.performance.common.exception.CustomException;
+import com.dayaeyak.performance.utils.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ApiResponse<?> handleCustomException(CustomException e){
+        return ApiResponse.error(e.getHttpStatus(), e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<?> handleNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining("\n"));
+        return ApiResponse.error(HttpStatus.BAD_REQUEST, errorMessage);
+
+    }
+}
